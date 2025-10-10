70f708caa1cd50f9e070ec1a86f8699ff66f1225
nana <160139950+anaalindoca@users.noreply.github.com>
2025-10-10 14:31:54 -0300

    ana

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
@@ -22,7 +23,7 @@ return dias + " dias "+ horas + " horas " + minutos +
 
 
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
